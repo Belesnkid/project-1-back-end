858b2bdf04c72162c489587d34072fbca1f91d42
Belesnkid <38082687+Belesnkid@users.noreply.github.com>
2022-01-18 22:24:18 -0500

    Logging Implemented
    Wrote Logger to write to different files depending on what is happening, need to be consistent with the logger.

--- errors/error-handler.ts
+++ errors/error-handler.ts
@@ -2,24 +2,33 @@ import {Response, Request} from "express";
 import ResourceNotFound from "./resource-not-found";
 import AlreadyExists from "./already-exists";
 import LoginError from "./login";
+import { logger, timestamp } from "..";
 
 export default function errorHandler(error: Error | unknown, req: Request, res: Response){
     if(error instanceof ResourceNotFound){
         console.log("Resource Could not be Found.");
+        logger.error(`${timestamp()} : Resource Could not be Found`);
+        logger.verbose(`${timestamp()} : Resource Could not be Found`);
         res.status(404);
         res.send(error.message);
     }
     else if(error instanceof AlreadyExists){
         console.log("Resource Already Exists");
+        logger.error(`${timestamp()} : Resource Already Exists`);
+        logger.verbose(`${timestamp()} : Resource Already Exists`);
         res.status(409);
         res.send(error.message);
     }
     else if (error instanceof LoginError){
         console.log("Invalid Login Information");
+        logger.error(`${timestamp()} : Invalid Login Provided`);
+        logger.verbose(`${timestamp()} : Invalid Login Provided`);
         res.status(401);
         res.send(error.message);
     }
     else{
+        logger.error(`${timestamp()} : An Unknown Error has Occured`);
+        logger.verbose(`${timestamp()} : An Unknown Error has Occured`);
         res.status(500);
         res.send("An Unknown Error has Occured");
     }
--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ import EmployeeServices, { EmployeeService } from "./services/employee-service";
 import cors from "cors";
 import LoginServiceImpl, { Login } from "./services/login-service";
 import ResourceNotFound from "./errors/resource-not-found";
+import winston from 'winston';
 
 const app = Express();
 app.use(Express.json());
@@ -16,18 +17,56 @@ const port: number = 3001;
 const employeeDao: EmployeeDAO = new LocalEmployeeDAO();
 const employeeServices: EmployeeService = new EmployeeServices(employeeDao);
 const loginService:Login = new LoginServiceImpl(employeeDao);
+const date = new Date();
+
+const levels = {
+    levels: {
+        error:0,
+        http:1,
+        info:2,
+        verbose:3
+    },
+    colors: {
+        error:'red',
+        http:'blue',
+        info:'green',
+        verbose:'white'
+    }
+}
+
+export const logger = winston.createLogger({
+    levels:levels.levels,
+    format:winston.format.json(),
+    transports:[
+        new winston.transports.File({filename:"./logs/error-log.txt", level:'error'}),
+        new winston.transports.File({filename:"./logs/http-log.txt", level:'http'}),
+        new winston.transports.File({filename:"./logs/info-log.txt", level:'info'}),
+        new winston.transports.File({filename:"./logs/verbose-log.txt", level:'verbose'})
+    ]
+})
+
+export function timestamp():String{
+    const [month, day, year] = [(date.getMonth() + 1), date.getDate(), date.getFullYear()];
+    const [hour, minute, second] = [date.getHours(), date.getMinutes(), date.getSeconds()];
+    return `${month}/${day}/${year} ${hour}:${minute}:${second}`;
+}
 
 app.patch("/login", async (req,res)=>{
     try{
+        logger.http(`${timestamp()} : Login Request`)
+        logger.verbose(`${timestamp()} : Login Request`);
         const body:{ username:string, pass:string } = req.body;
         const employee:Employee = await loginService.login(body.username,body.pass);
         console.log("Logged in successfully.");
+        logger.info(`${timestamp()} : ${employee.uName} successfully logged in`);
+        logger.verbose(`${timestamp()} : ${employee.uName} successfully logged in`);
         res.status(201);
         res.send(employee);
     } catch (error) {
         if (error instanceof ResourceNotFound){
-            console.log(true);
             console.log("Invalid Login Information");
+            logger.error(`${timestamp()} : Invalid Login Attempt; Username:${req.body.username}`);
+            logger.verbose(`${timestamp()} : Invalid Login Attempt; Username:${req.body.username}`);
             res.status(401);
             res.send(error.message);
         }
@@ -40,8 +79,10 @@ app.patch("/login", async (req,res)=>{
 //gets a list of all employees
 app.get("/employees", async (req, res) => {
     try {
+        logger.http(`${timestamp()} : Request for all Employees`);
         const employees: Employee[] = await employeeServices.retrieveAllEmployees();
         console.log("Retrieved all Employees from Database");
+        logger.info(`${timestamp()} : Got all Employees from the database`);
         res.status(200);
         res.send(employees);
     } catch (error) {
